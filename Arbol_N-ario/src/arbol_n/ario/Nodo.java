056b614501430b4833ba825b3c8e6bb8f46c3f5a
JasonEstrada <98917369+JasonEstrada@users.noreply.github.com>
2022-09-03 00:38:10 -0500

    Add files via upload

---
+++ Arbol_N-ario/src/arbol_n/ario/Nodo.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arbol_n.ario;
+
+import java.awt.Point;
+
+/**
+ *
+ * @author F-E-R
+ */
+public class Nodo {
+
+    private String info;
+    private int canhijos;
+    public Nodo hijos[];
+    private Nodo hijosT[];
+    public Point punto = new Point();
+
+    public Nodo(String dato) {
+        info = dato;
+        this.canhijos = 0;
+    }
+
+    private void copiarHijos() {
+        hijosT = new Nodo[canhijos + 1];
+        for (int i = 0; i < this.canhijos; i++) {
+            hijosT[i] = hijos[i];
+        }
+    }
+
+    public void aumentarHijos(Nodo nodo) {
+        copiarHijos();
+        hijosT[this.canhijos] = nodo;
+        hijos = hijosT;
+        this.canhijos++;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public int getCanhijos() {
+        return canhijos;
+    }
+}
